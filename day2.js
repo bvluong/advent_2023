ab904a1a8a2dafe5dc11813c24da5cf7ae45e0f9
Bryant Luong <bryantvinluong@gmail.com>
2023-12-01 22:56:55 -0800

    Update day2.js

--- day2.js
+++ day2.js
@@ -1,19 +1,9 @@
-const fs = require('fs');
-
-const input = fs.readFileSync('./input.txt', 'utf8').trimEnd();
-
-const map = {
-  green: 13,
-  red: 12,
-  blue: 14,
-}
-
 
 function solve(input) {
   const rows = input.split('\n');
-  let ans = []
+  let values = []
   rows.forEach((row) => {
-    let ansKey = {
+    let minValues = {
       green: 0,
       red: 0,
       blue: 0
@@ -22,28 +12,19 @@ function solve(input) {
     let bag = value.replaceAll(';', ',')
     bag.split(', ').forEach(item => {
       [num, color] = item.split(' ');
-      if (Number(num) > ansKey[color]) {
-        ansKey[color] = Number(num);
+      if (Number(num) > minValues[color]) {
+        minValues[color] = Number(num);
       }
     })
-    ans.push(ansKey);
+    values.push(minValues);
   })
   let sum = 0;
-  ans.forEach(row => {
+  powerList.forEach(row => {
     let power = Object.values(row).reduce((acc,curr) => {
       return acc * Number(curr); 
     }, 1)
-    console.log(row);
-    console.log(power);
     sum += power
   })
-  console.log(ans);
   console.log(sum);
-  // let result = ans.reduce((acc, curr) => {
-  //   const [y,num] = curr.split(' ')
-  //   return acc += Number(num);
-  // },0)
-  // console.log(result)
-  // console.log(rows);
 }
 solve(input);
